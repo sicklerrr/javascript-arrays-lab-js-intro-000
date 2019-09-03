555792d7588e1bf0cf339acefa6396abe6322da1
Aaron Sickler <sicklera517@gmail.com>
2019-09-03 20:53:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,6 +25,10 @@ function prependKitten(name){
   return [name, ...kittens]
 }
 
-function removeLastKitten(){
+function removeFirstKitten() {
   return kittens.slice(1)
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
 }
